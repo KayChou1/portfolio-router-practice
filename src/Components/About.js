6d23743bc85c2f6fca9bafe120fbf7c529e1e2ee
unknown <kayajohnson71@yahoo.com>
2020-08-04 11:02:17 -0400

    Updated About me

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,10 +5,12 @@ function About(){
     return(
         <>
             <p>
-                About Me!
+            Hello, Im Kaya!
             </p>
 
-            <p>I am currently a student studying Software Engineering and Computer Programming aspiring to be a Full Stack Developer. 
+            <p>                           
+                Im a Software Engineer and Computer Programmer. Soon to be a Full Stack Developer. 
+                Im always working on my Computer Programming skills to stay up to date with innovating technology.
                 I have previous college experience studying Business Administration. 
                 I am passionate about researching and learning new things. Technology is always innovating I am excited to be a part of it. 
                 I look forward to strengthening my developing skills. </p>
